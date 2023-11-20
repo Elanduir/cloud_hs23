5053af1ecb58c7149e1df5dc93a22e34693f379b
fabian <elanduir@gmail.com>
2023-11-20 10:49:47 +0100

    default runtime set

--- docker/runtime.sh
+++ docker/runtime.sh
@@ -1,3 +1,6 @@
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo apt install kubeadm
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt install kubeadm -y
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo systemctl restart containerd
