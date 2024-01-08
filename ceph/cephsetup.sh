90dfae0adb8e944a9507ef35163359e6aa68a99f
fabian <elanduir@gmail.com>
2024-01-08 09:48:06 +0100

    added setup

---
+++ ceph/cephsetup.sh
@@ -0,0 +1,9 @@
+curl --silent --remote-name --location https://github.com/ceph/ceph/raw/quincy/src/cephadm/cephadm
+sudo chmod +x cephadm
+sudo ./cephadm add-repo --release quincy
+sudo ./cephadm install
+echo export PATH="/usr/sbin/:$PATH" >> ~/.bashrc
+source ~/.bashrc
+
+
+sudo apt install lvm2 python3 podman -y 
